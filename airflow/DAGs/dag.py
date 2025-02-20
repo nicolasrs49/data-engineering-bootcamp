6b2489c9c8e9913200c7ddfd5b4217ba89ee35fc
Nicolas Santos <nicolasoliveira.santos@hotmail.com>
2025-02-20 09:06:06 -0300

    Script da DAG 'postgres_to_snowflake' corrigida

--- airflow/DAGs/dag.py
+++ airflow/DAGs/dag.py
@@ -30,31 +30,34 @@ def postgres_to_snowflake_etl():
             with SnowflakeHook(snowflake_conn_id='snowflake').get_conn() as conn:
                 with conn.cursor() as cursor:
                     query = f'SELECT MAX(id_{table_name}) FROM {table_name}'
-                    max_id = cursor.fetchone()[0]
                     cursor.execute(query)
+                    max_id = cursor.fetchone()[0]
                     return max_id if max_id is not None else 0
         
         @task(task_id=f'load_data_{table_name}')
         def load_incremental_data(table_name:str,max_id:int):
             with PostgresHook(postgres_conn_id='postgres').get_conn() as pgconn:
-                with pg_conn.cursor() as pg_cursor:
+                with pgconn.cursor() as pg_cursor:
                     primary_key = f'id_{table_name}'
                     
                     # Pega o nome das colunas
-                    query = f"SELECT column_name FROM information_schema WHERE table_name = '{table_name}'"
+                    query = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'"
                     pg_cursor.execute(query)
                     columns = [row[0] for row in pg_cursor.fetchall()]
                     columns_list_str = ', '.join(columns)
                     placeholders = ', '.join(['%s'] * len(columns)) # Isso monta o insert especial: INSERT INTO veiculos, vendas, estados... VALUES(%s, %s, %s, ...)
 
+                    # verifica se existem novos dados testando se o id do postgres é maior (mais recente) que o id do snowflake
                     query = f"SELECT {columns_list_str} FROM {table_name} WHERE {primary_key} > {max_id}"
+                    pg_cursor.execute(query)
                     rows = pg_cursor.fetchall()
 
+                    # Monta o insert dos dados no snowflake
                     with SnowflakeHook(snowflake_conn_id='snowflake').get_conn() as sf_conn:
                         with sf_conn.cursor() as sf_cursor:
                             insert_query = f"INSERT INTO {table_name} ({columns_list_str}) VALUES ({placeholders})"
                             for row in rows:
-                                sf_cursor.execute(insert_query)
+                                sf_cursor.execute(insert_query, row)
 
         # executa as funções armazenando os resultados
         max_id = get_max_primary_key(table_name)
